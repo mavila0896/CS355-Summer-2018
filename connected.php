2d174087c258e3c2f9024e89323e1017de54f1e0
The Doctor <thedoctor@The-Doctors-MBP.fios-router.home>
2019-01-21 22:07:47 -0500

    This is an update

---
+++ connected.php
@@ -0,0 +1,8 @@
+<html>
+<body background="bttfwall.png">
+<?php
+$name=$_POST["name"];
+    echo "<p> Now we're talking! ". $name. " has successfully contacted the server.<p>";
+?>
+</body>
+</html>
---
+++ tardisconnect.php
@@ -0,0 +1,6 @@
+<body background="tardiswall.png">
+<?php
+$name=$_POST["name"];
+    echo "<p> Hello Friend! ". $name. " has successfully contacted The Doctor's server.<p>";
+?>
+</body>
